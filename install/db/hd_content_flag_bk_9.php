b7fd264445b360dc56a4aa27f6a6bd2c3f5cba22
后盾网开源项目 <houdunwangxj@gmail.com>
2014-01-24 17:34:24 +0800

    常规修改

---
+++ install/db/hd_content_flag_bk_9.php
@@ -0,0 +1,4 @@
+<?php if(!defined('HDPHP_PATH'))EXIT;
+$db->exe("REPLACE INTO ".$db_prefix."content_flag (`aid`,`fid`,`cid`) VALUES('17','4','10')");
+$db->exe("REPLACE INTO ".$db_prefix."content_flag (`aid`,`fid`,`cid`) VALUES('16','4','10')");
+$db->exe("REPLACE INTO ".$db_prefix."content_flag (`aid`,`fid`,`cid`) VALUES('17','5','10')");
