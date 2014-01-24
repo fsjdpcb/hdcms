c34e692ea4f4324f682623eac2b7e166369d053f
后盾网开源项目 <houdunwangxj@gmail.com>
2014-01-24 17:33:36 +0800

    常规修改

--- install/db/hd_search_bk_21.php
+++
@@ -1,7 +0,0 @@
-<?php if(!defined('HDPHP_PATH'))EXIT;
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('3','请输入关键字a...','3')");
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('17','王','5')");
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('14','5','9')");
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('19','','2')");
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('20','v','1')");
-$db->exe("REPLACE INTO ".$db_prefix."search (`sid`,`name`,`total`) VALUES('22','后','2')");
