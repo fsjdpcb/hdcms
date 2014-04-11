11898f8fe50718759e5f340490d1d975ff22465b
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-04-11 11:04:15 +0800

    修复入口文件

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//define("DEBUG", true);
 date_default_timezone_set('PRC');
 //应用组目录
 define("GROUP_PATH", 'hd/');
@@ -7,7 +6,4 @@ define("GROUP_PATH", 'hd/');
 define('CACHE_PATH', 'data/cache/data/');
 //Temp目录
 define("TEMP_PATH", 'temp/');
-
-// require "hd/hdphp/hdphp/hdphp.php";
-
 require "hd/HDPHP/hdphp/hdphp.php";
