2766fac2a7b7f5db0b97e881eb5967ed5b664020
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-04-14 10:34:58 +0800

    更新入口文件

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-//define("DEBUG",true);
 date_default_timezone_set('PRC');
 //应用组目录
 define("GROUP_PATH", 'hd/');
@@ -7,6 +6,4 @@ define("GROUP_PATH", 'hd/');
 define('CACHE_PATH', 'data/cache/data/');
 //Temp目录
 define("TEMP_PATH", 'temp/');
-//require "hd/HDPHP/hdphp/hdphp.php";
-
-require "../hdphp/hdphp/hdphp.php";
+require "hd/HDPHP/hdphp/hdphp.php";
