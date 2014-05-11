d264e1ed9fa98ba98844e473a19136a5bf15c30f
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-05-11 21:59:57 +0800

    更新安装sql

---
+++ install/db/hd_template_bk_25.php
@@ -0,0 +1,2 @@
+<?php if(!defined('HDPHP_PATH'))EXIT;
+$db->exe("REPLACE INTO ".$db_prefix."template (`aid`,`cid`,`title`,`flag`,`new_window`,`seo_title`,`thumb`,`click`,`source`,`redirecturl`,`html_path`,`allowreply`,`addtime`,`updatetime`,`color`,`template`,`url_type`,`arc_sort`,`content_state`,`keywords`,`description`,`uid`,`favorites`,`comment_num`,`tag`,`read_credits`,`about`,`pics`,`tplfile`,`tel`,`qq`,`email`,`help`) VALUES('22','13','企业站模板','','0','','','0','','','','1','0','1399805765','','','3','0','1','','','1','0','0','','0','','','','','','','')");
