4314d89bebbdaea79db0bf6d4b95e5d157abb400
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-08-19 23:35:23 +0800

    更新入口文件

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ date_default_timezone_set('PRC');
 if (isset($_GET['app'])) {
     define('APP_PATH', 'hdcms/Addons/');
 } else {
-    define('APP_PATH', 'hdcms/');
+    define('APP_PATH', 'HDCMS/');
 }
 //临时目录
-define('TEMP_PATH', 'temp/');
+define('TEMP_PATH', 'Temp/');
 //插件目录
 define('APP_ADDON_PATH','Addons/');
 //引入框架主文件
