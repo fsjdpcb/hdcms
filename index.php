ab4d233c7a367a7afeb30fced11857d87f32e99c
houdunwangxj@gmail.com <houdunwangxj@gmail.com>
2014-09-10 00:03:27 +0800

    修改TEMP目录位置

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ date_default_timezone_set('PRC');
 //应用目录
 define('APP_PATH','HDCMS/');
 
+//Temp目录
+define('TEMP_PATH','Temp/');
+
 //引入框架主文件
-//require 'HDCMS/hdphp/hdphp.php';
-require '../hdphp/hdphp/hdphp.php';
+require 'HDCMS/hdphp/hdphp.php';
